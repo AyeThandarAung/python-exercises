9af8cb92cd1a45ced13c52c9869ef6d7f6e29754
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-06-08 12:30:00 +0530

    exe 13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script =argv
+apple = argv
+orange = argv
+grapefruit = argv
+
+print ("The script is called:" ,script)
+print ("Your first variable is:", apple)
+print ("Your second variable is:" , orange)
+print ("Your third variable is :", grapefruit)
