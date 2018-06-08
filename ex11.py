f646d46f0d7ed104000b99d8936aca4e8dc75eab
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-06-08 12:04:37 +0530

    exe 11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print ("How ole are you?"),
+age = raw_input('Enter your age')
+print ("How tall are you?"),
+height = raw_input('Enter you height')
+print ("How much do you weight?"),
+weight = raw_input('Enter your weight') 
+
+print ("So, you're %r old, %r tall and %r heavy." % (age, height,weight))
