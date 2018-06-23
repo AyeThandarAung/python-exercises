80c387f367746b85146ed3c07aedaa0755c8c5e6
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-06-23 16:01:09 +0530

    Parameters,Unpacking

--- ex13.py
+++ ex13.py
@@ -5,7 +5,7 @@ apple = argv
 orange = argv
 grapefruit = argv
 
-print ("The script is called:" ,script)
-print ("Your first variable is:", apple)
-print ("Your second variable is:" , orange)
-print ("Your third variable is :", grapefruit)
+print("The script is called:",script)
+print("Your first variable is:", apple)
+print("Your second variable is:", orange)
+print("Your third variable is :", grapefruit)
