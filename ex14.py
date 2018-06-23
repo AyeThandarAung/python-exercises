e006c2330f4326f6445966caaecd5f4ed3886424
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-06-23 16:21:10 +0530

    Prompting and Passing

--- ex14.py
+++ ex14.py
@@ -1,22 +1,23 @@
 from sys import argv
+from typing import List, Any
 
 script = argv
-user_name =argv
-prompt ='>'
+user_name = argv
+prompt = '>'
 
-print  ("Hi %s , I'm the %s script." % (user_name, script))
-print ("I'd like to ask you a few questons.")
-print ("Do you like me %s?" % user_name)
-likes = int (raw_input(prompt))
+print(f"Hi {user_name} , I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print("Do you like me {user_name}?")
+likes = input(prompt)
 
-print ("Where do you live %s?" % user_name)
-lives = int (raw_input(prompt))
+print("Where do you live {user_name}?")
+lives = input(prompt)
 
-print ("What kind of computer do you have?")
-computer = int (raw_input(prompt))
+print("What kind of computer do you have?")
+computer = input(prompt)
 
-print ("""
-        Alight, so you said %r about liking me.
-        You live in %r. Not sure where that is.
-        And you have a %r computer. Nice.
-        """ % (likes, lives, computer))
+print("""
+        Alight, so you said {likes} about liking me.
+        You live in {lives}. Not sure where that is.
+        And you have a {computer} computer. Nice.
+        """)
